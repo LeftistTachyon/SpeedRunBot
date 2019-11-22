dfc7da88fd95d0a49323c0fe581d34b0ce4ebadc
Gui Guy <jedandeganwang1999@gmail.com>
2019-11-21 21:08:45 -0600

    Initial commit :tada:

---
+++ src/main/java/com/leftisttachyon/speedrunbot/Main.java
@@ -0,0 +1,48 @@
+package com.leftisttachyon.speedrunbot;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.security.auth.login.LoginException;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+/**
+ * The main class for this application.
+ *
+ * @author LeftistTachyon
+ */
+public class Main extends ListenerAdapter {
+
+    /**
+     * The main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            JDABuilder builder = new JDABuilder(AccountType.BOT);
+            builder.setToken("NjQ3MjY5MzgzMzQ1MDEyNzM2.XddPaw.BWl2ItiYjX1LD4MK7-rn_zMuGeI");
+            builder.addEventListeners(new Main());
+            builder.build();
+        } catch (LoginException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("We received a message from " + 
+                event.getAuthor().getName() + ": " + 
+                event.getMessage().getContentDisplay());
+        
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+        
+        if (event.getMessage().getContentRaw().equals("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
+    }
+}
