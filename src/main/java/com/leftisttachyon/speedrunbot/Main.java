e2a08fa0175a2341ab642323e8e1ed5964519092
Gui Guy <jedandeganwang1999@gmail.com>
2019-12-01 16:40:14 -0600

    local changes, minor

--- src/main/java/com/leftisttachyon/speedrunbot/Main.java
+++ src/main/java/com/leftisttachyon/speedrunbot/Main.java
@@ -1,13 +1,13 @@
 package com.leftisttachyon.speedrunbot;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The main class for this application.
@@ -15,6 +15,11 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  * @author LeftistTachyon
  */
 public class Main extends ListenerAdapter {
+    
+    /**
+     * The logger for this class.
+     */
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
 
     /**
      * The main method
@@ -26,20 +31,23 @@ public class Main extends ListenerAdapter {
             JDABuilder builder = new JDABuilder(AccountType.BOT);
             builder.setToken(Token.getToken());
             builder.addEventListeners(new Main());
+            logger.info("JDABuilder instantiated!");
             builder.build();
+            logger.info("JDABuilder running....");
         } catch (LoginException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("The main method threw an exception: " 
+                    + ex.getClass().getSimpleName(), ex);
         }
     }
 
     /**
      * Called when a message is received on any accessible text channel.
      *
-     * @param event a {@code MessageReceivedEvent}F that describes the event
+     * @param event a {@code MessageReceivedEvent} that describes the event
      */
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        System.out.println("We received a message from "
+        logger.info("Message received from "
                 + event.getAuthor().getName() + ": "
                 + event.getMessage().getContentDisplay());
 
@@ -48,7 +56,7 @@ public class Main extends ListenerAdapter {
         }
 
         String[] data = event.getMessage().getContentRaw().split("\\s+");
-        if (data.length < 1 || !data[0].startsWith("~")) {
+        if (data.length < 1 || !data[0].startsWith("~")) { // prefix is "~"
             return;
         }
 
